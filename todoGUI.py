b8eddadc574ff081fcb87ea4e76ed96743b3aecf
calvin_udemy_python <cstephenson882@gmail.com>
2023-08-07 13:59:04 -0500

    Updated the Edit

--- todoGUI.py
+++ todoGUI.py
@@ -3,23 +3,52 @@ import PySimpleGUI as sg
 import functions
 
 enter_toDo_label = sg.Text('Type a to-do: ')
-enter_toDo_textBox = sg.InputText(tooltip='Enter ToDo', key='to-do') #tooltip fir hover mouse
+enter_toDo_textBox = sg.InputText(tooltip='Enter ToDo', key='todos') #tooltip fir hover mouse
 add_toDo_button = sg.Button("Add")
+list_box = sg.Listbox(values = functions.readToDO(),key ='todo_table',enable_events=True,size=[45,10])
+edit_button = sg.Button("Edit")
 
-layout = [ [enter_toDo_label,enter_toDo_textBox,add_toDo_button]
+layout = [ [enter_toDo_label,enter_toDo_textBox,add_toDo_button],
+           [list_box,edit_button]
 
          ]
+print(list_box)
+
 window  = sg.Window('To-Do App', layout)
 
+todos = functions.readToDO()
 while True:
+
     event, value = window.read()
+    print(event)
+    print(value)
+
     match event:
         case 'Add':
-            todos = functions.readToDO()
-            todos.append(value['to-do'] + '\n')
+            todos.append(value['todos'] + '\n')
             functions.writeToDo(todos)
+            window['todo_table'].update(values=todos)
+
+        case "Edit":
+            try:
+                edited_todo = value['todos']
+                print(todos)
+                index_todo = todos.index(value['todo_table'][0])
+                todos[index_todo] = edited_todo + '\n'
+                functions.writeToDo(todos)
+                window['todo_table'].update(values=todos)
+
+            except IndexError:
+                continue
+
+        case 'Complete':
+            continue
+        case 'todo_table':
+            window['todos'].update(value = value['todo_table'][0])
         case sg.WINDOW_CLOSED:
             break
+        #case True:
+
 window.close()
 
 
